ab8e653bcbc2bbd30ef8583bf0e9cf76b59460a2
kazurayam <kazuaki.urayama@gmail.com>
2018-11-22 22:46:42 +0900

    first commit

---
+++ src/test/java/effectivejava/chapter7/Item42PreferLambdasToAnonymousClassesTest.java
@@ -0,0 +1,19 @@
+package effectivejava.chapter7;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Effective Java, Chapter 7 Lambdas and Streams,
+ * Item 42 Prefer lambdas to annonymous classes
+ */
+public class Item42PreferLambdasToAnonymousClassesTest {
+
+    @Test
+    public void testSmoke() {
+
+    }
+}
